3c720dd07c57763140a382402c894ccabd6c28cb
Mattias Horvath <mh@4myhealth.at>
2025-04-27 10:58:27 +0200

    wip

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("mini.ai").setup({ n_lines = 500 })
 		require("mini.surround").setup()
-		require("mini.cursorword").setup()
+		-- require("mini.cursorword").setup()
 		require("mini.move").setup({
 			mappings = {
 				-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
